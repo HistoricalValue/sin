4544fe5cc9be318948e084b689897ddccd67aa39
koutsop <>
2009-06-11 15:29:27 +0000

    Changed the name of our language from MHN to SIN.

--- MHN/MHNmain/Src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "MHNmain/WorldGreeter.h"
-#include "MHN/VisitableTree.h"
-#include "MHNmain/Testers.h"
-
-int main(int argc, char *argv[]) {
-    MHNMain::MHN_VisitableTree_Tester::GetInstance()();
-    return MHNmain::WorldGreeter().Greet();
-}
