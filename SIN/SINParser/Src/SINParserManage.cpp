96137d747749fb67dc2e37576a9997c48197fc36
ilapost <>
2009-07-14 15:45:34 +0000

    Changed ASTNode to OpASTNode for Manage expr op expr functions

--- SIN/SINParser/Src/SINParserManage.cpp
+++ SIN/SINParser/Src/SINParserManage.cpp
@@ -131,135 +131,153 @@ namespace SIN {
 	///////////////////////////////////////////////////////////
 	// Manage Expressions
 	
-	//-----------------------------------------------------------------
+	//---------------------------------------------------------------------
 
-	void ParserManage::Manage_Expression_AssignExpression (ASTNode *_assignexpr, ASTNode **_retexpr) 
-		{ *_retexpr = _assignexpr; }
-	
-	
-	//-----------------------------------------------------------------
+	void ParserManage::Manage_Expression_AssignExpression (ASTNode *_assignexpr, ASTNode **_retexpr){
 
-	void ParserManage::Manage_Expression_ExpressionADDExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("+");
-		**_retexpr << _expr1 << _expr2;	
+		*_retexpr = _assignexpr;
 	}
 
-	
-	//-----------------------------------------------------------------
+	//---------------------------------------------------------------------
 
-	void ParserManage::Manage_Expression_ExpressionMINExpression	(ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("-");
-		**_retexpr << _expr1 << _expr2;	
-	}
+	void ParserManage::Manage_Expression_ExpressionADDExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
 
-	
-	//-----------------------------------------------------------------
+		*_retexpr = new AddASTNode();
 
-	void ParserManage::Manage_Expression_ExpressionMULExpression	(ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("*");
 		**_retexpr << _expr1 << _expr2;
 	}
 
-	
-	//-----------------------------------------------------------------
+	//---------------------------------------------------------------------
 
-	void ParserManage::Manage_Expression_ExpressionDIVExpression	(ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("/");
-		**_retexpr << _expr1 << _expr2;	
-	}
+	void ParserManage::Manage_Expression_ExpressionMINExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
 
-	
-	//-----------------------------------------------------------------
+		*_retexpr = new SubASTNode();
 
-	void ParserManage::Manage_Expression_ExpressionMODExpression	(ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("%");
-		**_retexpr << _expr1 << _expr2;	
-	}
-
-
-	//-----------------------------------------------------------------
-
-	void ParserManage::Manage_Expression_ExpressionGTExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode(">");
-		**_retexpr << _expr1 << _expr2;	
-	}
-
-	
-	//-----------------------------------------------------------------
-
-	void ParserManage::Manage_Expression_ExpressionGEExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode(">=");
-		**_retexpr << _expr1 << _expr2;	
-	}
-
-	
-	//-----------------------------------------------------------------
-
-	void ParserManage::Manage_Expression_ExpressionLTExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("<");
 		**_retexpr << _expr1 << _expr2;
 	}
 
-	
-	//-----------------------------------------------------------------
+	//---------------------------------------------------------------------
 
-	void ParserManage::Manage_Expression_ExpressionLEExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("<=");
+	void ParserManage::Manage_Expression_ExpressionMULExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
+
+		*_retexpr = new MulASTNode();
+
 		**_retexpr << _expr1 << _expr2;
 	}
 
+	//---------------------------------------------------------------------
 
-	//-----------------------------------------------------------------
+	void ParserManage::Manage_Expression_ExpressionDIVExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
 
-	void ParserManage::Manage_Expression_ExpressionEQExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("==");
-		**_retexpr << _expr1 << _expr2;	
+		*_retexpr = new DivASTNode();
+
+		**_retexpr << _expr1 << _expr2;
 	}
 
+	//---------------------------------------------------------------------
 	
-	//-----------------------------------------------------------------
+	void ParserManage::Manage_Expression_ExpressionMODExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
 
-	void ParserManage::Manage_Expression_ExpressionNOTEQExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("!=");
-		**_retexpr << _expr1 << _expr2;	
+		*_retexpr = new ModASTNode();
+
+		**_retexpr << _expr1 << _expr2;
 	}
 
+	//---------------------------------------------------------------------
 
-	//-----------------------------------------------------------------
+	void ParserManage::Manage_Expression_ExpressionGTExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
 
-	void ParserManage::Manage_Expression_ExpressionANDExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("and");
-		**_retexpr << _expr1 << _expr2;	
+		*_retexpr = new GtASTNode();
+
+		**_retexpr << _expr1 << _expr2;
 	}
 
+	//---------------------------------------------------------------------
 
-	//-----------------------------------------------------------------
+	void ParserManage::Manage_Expression_ExpressionGEExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
 
-	void ParserManage::Manage_Expression_ExpressionORExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
-		*_retexpr = new ASTNode("or");
-		**_retexpr << _expr1 << _expr2;	
+		*_retexpr = new GeASTNode();
+
+		**_retexpr << _expr1 << _expr2;
 	}
 
-	
-	//-----------------------------------------------------------------
+	//---------------------------------------------------------------------
 
-	void ParserManage::Manage_Expression_ExpressionNOTExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr) {
+	void ParserManage::Manage_Expression_ExpressionLTExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
+
+		*_retexpr = new LtASTNode();
+
+		**_retexpr << _expr1 << _expr2;
+	}
+
+	//---------------------------------------------------------------------
+
+	void ParserManage::Manage_Expression_ExpressionLEExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
+
+		*_retexpr = new LeASTNode();
+
+		**_retexpr << _expr1 << _expr2;
+	}
+
+	//---------------------------------------------------------------------
+
+	void ParserManage::Manage_Expression_ExpressionEQExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
+
+		*_retexpr = new EqASTNode();
+
+		**_retexpr << _expr1 << _expr2;
+	}
+
+	//---------------------------------------------------------------------
+
+	void ParserManage::Manage_Expression_ExpressionNOTEQExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
+
+		*_retexpr = new NeASTNode();
+
+		**_retexpr << _expr1 << _expr2;
+	}
+
+	//---------------------------------------------------------------------
+
+	void ParserManage::Manage_Expression_ExpressionANDExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
+
+		*_retexpr = new AndASTNode();
+
+		**_retexpr << _expr1 << _expr2;
+	}
+
+	//---------------------------------------------------------------------
+
+	void ParserManage::Manage_Expression_ExpressionORExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
+
+		*_retexpr = new OrASTNode();
+
+		**_retexpr << _expr1 << _expr2;
+	}
+
+	//---------------------------------------------------------------------
+
+	void ParserManage::Manage_Expression_ExpressionNOTExpression (ASTNode *_expr1, ASTNode *_expr2, ASTNode **_retexpr){
+
 		*_retexpr = new ASTNode("not");
+
 		**_retexpr << _expr1 << _expr2;
 	}
 
+	//---------------------------------------------------------------------
 
-	//-----------------------------------------------------------------
+	void ParserManage::Manage_Expression_MetaExpression (ASTNode *_metaexpr, ASTNode **_retexpr){
 
-	void ParserManage::Manage_Expression_MetaExpression (ASTNode *_metaexpr, ASTNode **_retexpr) 
-		{ *_retexpr = _metaexpr; }
+		*_retexpr = _metaexpr;
+	}
 
+	//---------------------------------------------------------------------
 
-	//-----------------------------------------------------------------
+	void ParserManage::Manage_Expression_Term (ASTNode *_term, ASTNode **_retexpr){
 
-	void ParserManage::Manage_Expression_Term (ASTNode *_term, ASTNode **_retexpr) {
 		SIN::Logger &logger = SIN::LoggerManager::SingletonGetInstance()->GetLogger("SIN::Tests::Parser::Manage");
 		logger.Notice("Entered expr : term Rule");
+
 		*_retexpr = _term;
 	}
 
