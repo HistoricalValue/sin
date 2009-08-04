ecab667248af6c7787972094f561a88a74dfd9b7
koutsop <>
2009-08-04 15:51:49 +0000

    Added two new SIN types (SINFucntion and SINLibFunction) but they do not work yet

---
+++ SIN/SINCommon/Include/SINFunction.h
@@ -0,0 +1,36 @@
+#ifndef __SIN_FUNCTION_H__
+#define __SIN_FUNCTION_H__
+
+#include "SINString.h"
+#include "SINASTNode.h"
+
+namespace SIN {
+	class SINFunction { 
+	public:
+
+		SINFunction(void);
+		SINFunction(ASTNode * _node) : node (_node) {}
+
+
+		~SINFunction();
+
+
+		const ASTNode *	GetASTNode(void) const;
+		const String &  GetName(void) const;
+
+		void SetName(const String &);
+		void SetASTNode(const ASTNode *);
+
+
+	private:
+		ASTNode	*	node;
+		
+	};
+
+	extern String const to_string(SIN::SINFunction const * val);
+	extern String const to_string(SIN::SINFunction const & val);
+}
+
+
+
+#endif
---
+++ SIN/SINCommon/Src/SINFucntion.cpp
@@ -0,0 +1,6 @@
+#include "SINFunction.h"
+
+
+namespace SIN {
+	
+}
