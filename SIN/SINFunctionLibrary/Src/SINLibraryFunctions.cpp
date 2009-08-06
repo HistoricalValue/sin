6881b965d9c2b42b14efab43c7ddc01d6afc91c0
rudolf <>
2009-08-06 20:22:33 +0000

    * Added libfuncs tostring(), strtonum(), strsavetofile() and totalarguments() * Enriched VirtualMachine a lot

--- SIN/SINFunctionLibrary/Src/SINLibraryFunctions.cpp
+++ SIN/SINFunctionLibrary/Src/SINLibraryFunctions.cpp
@@ -1,5 +1,6 @@
 #include "SINLibraryFunctions.h"
 #include "SINLibrary.h"
+#include "SINFileOutputStream.h"
 
 #define SIN_LIBRARYFUNCTIONS_LIBFUNC(FNAME) FNAME::return_type FNAME::operator ()(SIN_FUNCTIONLIBRARY_FUNC_ARGS) const
 
@@ -11,6 +12,7 @@ namespace SIN {
 				size_t const num_args = _st.NumberOfArguments();
 				for (size_t i = 0; i < num_args; i++)
 					_vm.Print(_st.Argument(i)->ToString());
+				_vm.ReturnValueNil();
 			}
 			// println ---------------------------------------------------------
 			SIN_LIBRARYFUNCTIONS_LIBFUNC(println) {
@@ -19,12 +21,34 @@ namespace SIN {
 				_lib.Invoke("print", _st);
 			}
 			// tostring ---------------------------------------------------------
-			//SIN_LIBRARYFUNCTIONS_LIBFUNC(tostring) {
-			//}
+			SIN_LIBRARYFUNCTIONS_LIBFUNC(tostring) {
+				if (_st.NumberOfArguments() > 0)
+					_vm.ReturnValueString(_st.Argument(0)->ToString());
+				else
+					_vm.AppendError("not enough arguments passed to tostring(obj)", "", 0u);
+			}
 			// strtonum ---------------------------------------------------------
-			//SIN_LIBRARYFUNCTIONS_LIBFUNC(strtonum);
+			SIN_LIBRARYFUNCTIONS_LIBFUNC(strtonum) {
+				if (_st.NumberOfArguments() > 0) {
+					double const num = strtod(_st.Argument(0)->ToString().c_str(), NULL);
+					_vm.ReturnValueNumber(num);
+				}
+				else
+					_vm.AppendError("not enough arguments passed to strtonum(str)", "", 0u);
+			}
 			// strsavetofile ----------------------------------------------------
-			//SIN_LIBRARYFUNCTIONS_LIBFUNC(strsavetofile);
+			SIN_LIBRARYFUNCTIONS_LIBFUNC(strsavetofile) {
+				if (_st.NumberOfArguments() > 1) {
+					FileOutputStream fout(_st.Argument(0)->ToString());
+					String const str(_st.Argument(1)->ToString());
+					char const* const c_str = str.c_str();
+					size_t const len = strlen(c_str);
+					fout.write(c_str, len);
+					_vm.ReturnValueNumber(len);
+				}
+				else
+					_vm.AppendError("not enough arguments passed to strsavetofile(file,str)", "", 0u);
+			}
 			// typeof -----------------------------------------------------------
 			//SIN_LIBRARYFUNCTIONS_LIBFUNC(typeof);
 			// input ------------------------------------------------------------
@@ -36,7 +60,9 @@ namespace SIN {
 			// writefile --------------------------------------------------------
 			//SIN_LIBRARYFUNCTIONS_LIBFUNC(writefile);
 			// totalarguments ---------------------------------------------------
-			//SIN_LIBRARYFUNCTIONS_LIBFUNC(totalarguments);
+			SIN_LIBRARYFUNCTIONS_LIBFUNC(totalarguments) {
+				_vm.ReturnValueNumber(_vm.Stack().top().f.GetASTNode()->LocalEnv()->NumberOfArguments());
+			}
 			// arguments --------------------------------------------------------
 			//SIN_LIBRARYFUNCTIONS_LIBFUNC(arguments);
 			// objectcopy -------------------------------------------------------
--- SIN/SINRuntime/Include/SINVirtualMachine.h
+++ SIN/SINRuntime/Include/SINVirtualMachine.h
@@ -4,6 +4,9 @@
 #include "SINMemoryCell.h"
 #include "SINMemoryCellNil.h"
 #include "SINMemoryCellString.h"
+#include "SINMemoryCellNumber.h"
+#include <list>
+#include <stack>
 
 namespace SIN {
 	namespace VM {
@@ -16,13 +19,41 @@ namespace SIN {
 
 			MemoryCell* ReturnValue(void) const { return retval; }
 			void ReturnValueNil(void) { retval = &nil; }
-			void ReturnValueString(String const& _s) { str = _s; }
+			void ReturnValueString(String const& _s) { str.SetValue(_s); retval = &str; }
+			void ReturnValueNumber(SIN::Types::Number_t const& _num) { num.SetValue(_num); retval = &num; }
 
+			struct Error {
+				String const message;
+				char const* file;
+				unsigned int line;
+				Error(String const& _message, char const* _file, unsigned int _line):
+					message(_message), file(_file), line(_line) { }
+			};
+
+			struct Frame {
+				SIN::Types::Function_t& f;
+				Frame(Types::Function_t& _f): f(_f) { }
+			};
+
+			typedef std::list<Error> errors_t;
+			void AppendError(String const& _message, char const* _file, unsigned int _line)
+				{ errors.push_back(Error(_message, _file, _line)); }
+			errors_t const& Errors(void) const { return errors; }
+			bool HasError(void) const { return errors.size() > 0; }
+
+			typedef std::stack<Frame> stack_t;
+			void PushFrame(Types::Function_t* _f_p) { stack.push(Frame(*_f_p)); }
+			void PopFrame(void) { stack.pop(); }
+			stack_t const& Stack(void) const { return stack; }
 		private:
 			print_handler_t print_handler;
 			MemoryCell* retval;
 			MemoryCellNil nil;
 			MemoryCellString str;
+			MemoryCellNumber num;
+
+			stack_t stack;
+			errors_t errors;
 		}; // class VirtualMachine
 	} // namespace VM
 } // namespace SIN
