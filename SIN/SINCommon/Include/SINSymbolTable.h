d2ed8bd3360e9e04319042d46cd87a03b8d1055d
rudolf <>
2009-08-26 19:18:34 +0000

    * SymbolTable API nice-ment

--- SIN/SINCommon/Include/SINSymbolTable.h
+++ SIN/SINCommon/Include/SINSymbolTable.h
@@ -13,18 +13,19 @@ namespace SIN {
 		typedef String						name_t;
 		typedef InstanceProxy<MemoryCell>	elem_t;
 
-		elem_t& Lookup(const name_t&);
-		elem_t& Lookup(const scope_id&, const name_t&);
-		elem_t& LookupOnlyInCurrentScope(const name_t&);
-		elem_t& LookupByIndex(unsigned int index);
+		elem_t& Lookup(const name_t&); // in current and smaller scopes
+		elem_t& Lookup(const scope_id&, const name_t&); // in given scope
+		elem_t& LookupOnlyInCurrentScope(const name_t&); // in current scope
+		elem_t& LookupByIndex(unsigned int index); // in current scope
 
-		void Insert(const name_t&, const elem_t&);
+		void Insert(const name_t&, const elem_t&); // only in current scope
 
 		void IncreaseScope(void);
 		void DecreaseScope(void);
 		const scope_id CurrentScope(void) const;
 
 		unsigned int NumberOfSymbols(void) const; // current scope
+		unsigned int NumberOfSymbols(const scope_id&) const; // in given scope
 
 		struct EntryHandler {
 			virtual bool operator ()(const name_t&, const elem_t&) { return false; }
