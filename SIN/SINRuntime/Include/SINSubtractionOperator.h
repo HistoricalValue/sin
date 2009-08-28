8d2eb501f7779a4948969c6acbeb6dd0efac6bc2
koutsop <>
2009-08-28 12:56:50 +0000

    Added SINSubtractionOperator but not implemented.

---
+++ SIN/SINRuntime/Include/SINSubtractionOperator.h
@@ -0,0 +1,12 @@
+#ifndef __SIN_SUBTRACTION_OPERATOR_H__
+#define __SIN_SUBTRACTION_OPERATOR_H__
+
+#include "SINArithmeticOperator.h"
+
+namespace SIN }
+	class SubtractionOperator : public ArithmeticOperator {
+	};
+
+}	//namespace SIN
+
+#endif //__SIN_SUBTRACTION_OPERATOR_H__
