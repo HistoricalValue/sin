69a8bc073d37aec9a60691fdbae8318038648da5
Rudolf Reindeer <rudolf@fakedream>
2009-09-03 03:01:19 +0300

    updated bazaar-pushall-script

--- SIN/bazaar_manager.bash
+++ SIN/bazaar_manager.bash
@@ -15,11 +15,11 @@ branches=(
 bzr pull 'https://139.91.186.143:8443/svn/CS-540/trunk'
 
 for branch in "${branches[@]}"
-do  bazaar_push "$branch"
+do  bazaar_push "$branch" &
 done
 
 if [ "$1" = 'all' ]
-then bazaar_push "$pseudosystems_branch" 
+then bazaar_push "$pseudosystems_branch"  &
 fi
 
 wait
